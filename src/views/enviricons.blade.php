18a20178396859fa2758e3254f125bc07a644973
wdmtech <willda>
2015-04-20 22:11:32 +0100

    Changes PSR-0 root in composer.json Fixes missing config namespaces in enviricons.blade.php

--- src/views/enviricons.blade.php
+++ src/views/enviricons.blade.php
@@ -1,16 +1,16 @@
 
 {{--Local--}}
-@if(App::environment() == Config::get('enviricons', 'local'))
+@if(App::environment() == Config::get('enviricons.environment.local', 'local'))
 
     <link rel="icon" href="packages/wdmtech/enviricons/local/favicon.ico" type="image/x-icon">
 
 {{--Dev--}}
-@elseif(App::environment() == Config::get('enviricons', 'dev'))
+@elseif(App::environment() == Config::get('enviricons.environment.dev', 'dev'))
 
     <link rel="icon" href="packages/wdmtech/enviricons/dev/favicon.ico" type="image/x-icon">
 
 {{--staging--}}
-@elseif(App::environment() == Config::get('enfavicons.environment.staging', 'staging'))
+@elseif(App::environment() == Config::get('enviricons.environment.staging', 'staging'))
 
     <link rel="icon" href="packages/wdmtech/enviricons/staging/favicon.ico" type="image/x-icon">
 
