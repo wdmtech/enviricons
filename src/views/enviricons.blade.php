0e811d8f6b06d88a0dcd1f89954cffcdfb14026a
wdmtech <willda>
2015-04-20 22:05:40 +0100

    Changes package name in composer.json Changes file paths to reflect package name change in enviricons.blade.php

--- src/views/enviricons.blade.php
+++ src/views/enviricons.blade.php
@@ -2,21 +2,21 @@
 {{--Local--}}
 @if(App::environment() == Config::get('enviricons', 'local'))
 
-    <link rel="icon" href="/packages/wdmtech/enfaviconsenviricons/favicons/local/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="packages/wdmtech/enviricons/local/favicon.ico" type="image/x-icon">
 
 {{--Dev--}}
 @elseif(App::environment() == Config::get('enviricons', 'dev'))
 
-    <link rel="icon" href="/favicons/dev/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="packages/wdmtech/enviricons/dev/favicon.ico" type="image/x-icon">
 
 {{--staging--}}
 @elseif(App::environment() == Config::get('enfavicons.environment.staging', 'staging'))
 
-    <link rel="icon" href="/favicons/staging/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="packages/wdmtech/enviricons/staging/favicon.ico" type="image/x-icon">
 
 {{-- Your default/live favicon  --}}
 @else
 
-    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="packages/wdmtech/enviricons/favicon.ico" type="image/x-icon">
 
 @endif
