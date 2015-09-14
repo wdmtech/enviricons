7186a06b988d359eb643d7969bb3204cf38816be
wdmtech <willdavidmurray@gmail.com>
2015-09-14 20:12:09 +0100

    Short arrays

--- src/Wdmtech/Enviricons/config/enviricons.php
+++ src/Wdmtech/Enviricons/config/enviricons.php
@@ -1,16 +1,16 @@
 <?php
 
-return array(
+return [
 
     // Simply change the values in the environments array to match
     // whatever you've specified in your bootstrap/start.php file
 
-    'environments' => array(
+    'environments' => [
 
         'local'   => 'local',   // Your local development environment name
         'dev'     => 'dev',     // ...dev
         'staging' => 'staging', // ...staging
         'live'    => 'live',    // ...live/default
-    )
+    ]
 
-);
+];
